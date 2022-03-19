c8544330014fb945a23230ff4c810d7805fb0981
DevSouza <devsouza01@gmail.com>
2022-03-19 12:45:57 -0300

    O Primeiro Microservice - AuthUser

---
+++ EAD/authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -0,0 +1,13 @@
+package com.ead.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApplication.class, args);
+	}
+
+}
---
+++ EAD/authuser/src/test/java/com/ead/authuser/AuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
