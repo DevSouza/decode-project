857bd8970f04125e8edaf9d94f3a4029d7318bb6
DevSouza <devsouza01@gmail.com>
2022-03-29 10:57:18 -0300

    Deleção de Relacionamento Cascade.ALL ou Método de Deleção Customizado

--- EAD/course/src/main/java/com/ead/course/services/CourseService.java
+++ EAD/course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,5 +1,9 @@
 package com.ead.course.services;
 
+import com.ead.course.models.CourseModel;
+
 public interface CourseService {
 
+	void delete(CourseModel courseModel);
+	
 }
--- EAD/course/src/main/java/com/ead/course/services/LessonService.java
+++ EAD/course/src/main/java/com/ead/course/services/LessonService.java
@@ -1,5 +1,9 @@
 package com.ead.course.services;
 
+import com.ead.course.models.LessonModel;
+
 public interface LessonService {
 
+	void delete(LessonModel lessonModel);
+	
 }
