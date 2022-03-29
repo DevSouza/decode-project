208e8d99b76acced91cc4648efb809781d329711
DevSouza <devsouza01@gmail.com>
2022-03-29 11:52:27 -0300

    Criar API RESTful para Course com Validação Spring Validation

--- EAD/course/src/main/java/com/ead/course/services/CourseService.java
+++ EAD/course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,9 +1,19 @@
 package com.ead.course.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.ead.course.models.CourseModel;
 
 public interface CourseService {
 
 	void delete(CourseModel courseModel);
+
+	CourseModel save(CourseModel courseModel);
+
+	Optional<CourseModel> findById(UUID courseId);
+
+	List<CourseModel> findAll();
 	
 }
