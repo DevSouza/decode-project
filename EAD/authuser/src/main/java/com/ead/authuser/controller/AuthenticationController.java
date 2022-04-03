9cb1a198bee08e963b5205d672623d7d0cd1fceb
DevSouza <devsouza01@gmail.com>
2022-04-03 13:49:33 -0300

    Principais Bibliotecas de Logging, Log Levels e Logging no Spring com Logback

--- EAD/authuser/src/main/java/com/ead/authuser/controller/AuthenticationController.java
+++ EAD/authuser/src/main/java/com/ead/authuser/controller/AuthenticationController.java
@@ -1,25 +1,36 @@
 package com.ead.authuser.controller;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.enuns.UserStatus;
 import com.ead.authuser.enuns.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+	
+	Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @Autowired
     UserService userService;
@@ -52,4 +63,15 @@ public class AuthenticationController {
                 .body(userModel);
     }
 
+    @GetMapping("/")
+    public String index() {
+    	logger.trace("TRACER"); // Log bem detalhado.
+    	logger.debug("DEBUG"); // Informações detalhadas para o desenvolvedor(Quando o projeto estiver em desenvolvimento).
+    	logger.info("INFO"); // Informações não tão detalhadas .
+    	logger.warn("WARN"); // Avisos sobre possiveis erros(Conflito, perda de dados secundarios, ...).
+    	logger.error("ERROR"); // Detalhar o erro ocorrido.
+    	
+    	return "Loggin Spring Boot...";
+    }
+    
 }
