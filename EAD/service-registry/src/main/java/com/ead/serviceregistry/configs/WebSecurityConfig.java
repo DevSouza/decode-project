6e67be075259539e89eda0b6abd2b27f1ccc0a6d
Andre Souza <devsouza01@gmail.com>
2022-06-16 15:09:49 -0300

    Implementar Basic Authentication no Service Registry

---
+++ EAD/service-registry/src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.ead.serviceregistry.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${ead.serviceRegistry.username}")
+	private String username;
+	
+	@Value("${ead.serviceRegistry.password}")
+	private String password;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.httpBasic()
+			.and()
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.csrf().disable()
+			.formLogin();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser(username)
+			.password(passwordEncoder().encode(password))
+			.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
