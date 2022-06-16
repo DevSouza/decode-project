bbac6495967d056abb61222cfe5919b903d3f336
Andre Souza <devsouza01@gmail.com>
2022-06-15 23:06:49 -0300

    Criar Endpoints para visualizar e atualizar Notifications

--- EAD/notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
+++ EAD/notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -1,10 +1,19 @@
 package com.ead.notification.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
 
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
+
+	Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
+
+	Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId);
+	
 }
--- EAD/notification/src/main/java/com/ead/notification/services/NotificationService.java
+++ EAD/notification/src/main/java/com/ead/notification/services/NotificationService.java
@@ -1,9 +1,19 @@
 package com.ead.notification.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ead.notification.models.NotificationModel;
 
 public interface NotificationService {
 
 	NotificationModel saveNotification(NotificationModel notificationModel);
+
+	Page<NotificationModel> findAllNotificationsByUser(UUID userId, Pageable pageable);
+
+	Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId);
 
 }
